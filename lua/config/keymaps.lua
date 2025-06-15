676b2492085ba0b5895d1531c7a5b3e8343e4d38
Ethan Haque <ethan.k.haque@gmail.com>
2025-06-15 01:22:27 -0400

    fix: remove telescope keybindings from keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,14 +9,6 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- Reload configuration without restart nvim
 vim.keymap.set("n", "<leader>r", ":so %<CR>", {})
 
--- TODO: add these to telescope configuration
--- Telescope
--- local builtin = require("telescope.builtin")
--- vim.keymap.set("n", "<leader>fd", builtin.find_files, {})
--- vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
--- vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
--- vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-
 -- Open explorer
 vim.keymap.set("n", "<leader>e", ":Ex<CR>", {})
 
