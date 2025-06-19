efc18a3bbea07353750ea2595caee1dba2f788c4
Ethan Haque <ethan.k.haque@gmail.com>
2025-06-19 02:14:40 -0400

    feat: add typescript and tsx as default treesitter grammars

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "typescript", "tsx"},
           sync_install = false,
           highlight = { enable = true, use_languagetree=true },
           indent = { enable = true },
