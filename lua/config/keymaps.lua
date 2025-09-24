069a080e84adbdb3b5cbfac647796b44eec87bfd
Ethan Haque <ethan.k.haque@gmail.com>
2025-09-24 11:02:13 -0400

    feat: remove toggleterm as dependency
    use `:vert term` or `vsp | term` and swap buffers instead

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,14 +1,5 @@
 vim.keymap.set("n", "<leader>q", ":qa!<CR>", {})
 
--- Keybinds to make split navigation easier.
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
--- Reload configuration without restart nvim
-vim.keymap.set("n", "<leader>r", ":so %<CR>", {})
-
 -- Open explorer
 vim.keymap.set("n", "<leader>e", ":Ex<CR>", {})
 
