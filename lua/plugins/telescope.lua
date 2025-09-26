1e3fcc6294391133f79ce44ac39e5d73b72e19b2
Ethan Haque <ethan.k.haque@gmail.com>
2025-09-25 22:43:28 -0400

    fix: add find_files options section

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,9 +3,16 @@ return {
 	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	keys = {
-		{ "<leader>fd", "<cmd>Telescope find_files --hidden<cr>", desc = "Find file" },
+		{ "<leader>fd", "<cmd>Telescope find_files<cr>", desc = "Find file" },
 		{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
 		{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
 		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tag" },
+	},
+	opts = {
+		pickers = {
+			find_files = {
+				find_command = { "rg", "--files", "--hidden", "--glob", "!.git" },
+			},
+		},
 	},
 }
