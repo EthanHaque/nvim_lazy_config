223bb660ce781298e32bb7c8c2b32bff29bdc069
Ethan Haque <ethan.k.haque@gmail.com>
2026-01-14 15:11:58 -0500

    refactor: remove open float keybind

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,10 +5,3 @@ vim.keymap.set("n", "<leader>e", ":Ex<CR>", {})
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
-vim.keymap.set(
-	"n",
-	"gl",
-	vim.diagnostic.open_float,
-	{ noremap = true, silent = true, desc = "Open floating diagnostic" }
-)
