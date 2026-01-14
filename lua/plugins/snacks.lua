0e8b9d1f6317d2dddae7e0fcd1f19ebaa3312355
Ethan Haque <ethan.k.haque@gmail.com>
2026-01-14 16:18:56 -0500

    feat: replace telescope and gitsigns with snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,27 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		gh = { enabled = true },
+		image = { enabled = true },
+		picker = { enabled = true },
+		quickfile = { enabled = true },
+		statuscolumn = { enabled = true },
+	},
+	keys = {
+        -- stylua: ignore start
+        { "<leader>fd", function() Snacks.picker.files() end, desc = "Find Files" },
+        { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep Text" },
+        { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+        { "<leader>fh", function() Snacks.picker.help() end, desc = "Help Tags" },
+        { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent Files" },
+
+        { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
+        { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
+        { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
+        { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
+		-- stylua: ignore end
+	},
+}
