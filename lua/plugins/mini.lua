ae4bc5fbeca65d04722a0a696f410f051d5a8c5e
Ethan Haque <ethan@tradespace.io>
2026-01-26 14:59:20 -0500

    fix: replace nvim-web-devicons with mini.icons

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,14 +1,1 @@
-return {
-	"echasnovski/mini.icons",
-	opts = {},
-	lazy = true,
-	specs = {
-		{ "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
-	},
-	init = function()
-		package.preload["nvim-web-devicons"] = function()
-			require("mini.icons").mock_nvim_web_devicons()
-			return package.loaded["nvim-web-devicons"]
-		end
-	end,
-}
+return { "echasnovski/mini.icons", lazy = true }
