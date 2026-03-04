c713495c2661814b67de3275d8edd8f4803f163a
Ethan Haque <ethan.k.haque@gmail.com>
2026-03-04 16:01:26 -0500

    feat: add surround color commands

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -1,3 +1,19 @@
 vim.opt_local.wrap = true
 vim.opt_local.spell = true
 vim.opt_local.linebreak = true
+
+require("nvim-surround").buffer_setup({
+	surrounds = {
+		["C"] = {
+			add = function()
+				local config = require("nvim-surround.config")
+				local color = config.get_input("Color: ")
+				if color then
+					return { { "#text(fill: " .. color .. ")[" }, { "]" } }
+				end
+			end,
+			find = "#text$(fill: .-%)%[.-%]",
+			delete = "^(#text%(fill: .-%)%[)().-(%])()$",
+		},
+	},
+})
